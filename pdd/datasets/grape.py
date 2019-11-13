9d76fce3095dea20fc6aa0fe7c7a71b8efe5841d
Yasser Abdel gaber <57070084+Yasser144@users.noreply.github.com>
2019-11-13 11:21:53 +0200

    Update grape.py

--- pdd/datasets/grape.py
+++ pdd/datasets/grape.py
@@ -25,8 +25,8 @@ def load_data(path='crops_nn.tar',
         Path to the folder with data or tuple with train and test paths
     """
     path = get_file(path,
-                    origin="http://pdd.jinr.ru/crops_nn.tar",
-                    file_hash='cbc74aef6e5f20ef22b41d1f0f77e40567e6fd1f4e2c007341df9fb4672c3fd8',
+                    origin="http://pdd.jinr.ru/crops.php?lang=en",
+                    
                     extract=True)
 
     try:
