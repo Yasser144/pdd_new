b885412602ec119ddc59ef8e41ab32959e39e491
Yasser Abdel gaber <57070084+Yasser144@users.noreply.github.com>
2019-11-21 10:23:06 +0200

    Update grape.py

--- pdd/datasets/grape.py
+++ pdd/datasets/grape.py
@@ -10,7 +10,7 @@ import numpy as np
 import sys
 
 
-def load_data(path='tomato', 
+def load_data(path='tomato.tar', 
               split_on_train_test=False, 
               test_size=None, 
               random_state=0):
